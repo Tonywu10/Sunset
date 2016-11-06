e5ef1c86029c6814937fcd54373cac97759a8e8b
Tonywu10 <tonywu1061@gmail.com>
2016-11-06 13:55:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tonywu10/sunset/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.tonywu10.sunset;
+
+import android.support.v4.app.Fragment;
+
+public class MainActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+}
